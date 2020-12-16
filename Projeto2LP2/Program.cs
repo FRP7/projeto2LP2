a0063692b88b0cfba5eb16b7ea408ad288e1d0d3
FranciscoPires <frelvaspires@gmail.com>
2020-12-16 18:44:30 +0000

    A solução já renderiza o cenário do jogo.

--- Projeto2LP2/Program.cs
+++ Projeto2LP2/Program.cs
@@ -10,9 +10,12 @@ namespace Projeto2LP2
     class Program
     {
         static void Main(string[] args) {
-            Menu menu = new Menu();
-            Console.WriteLine("Entrar na solução.");
-            menu.MainMenu();
+            //Menu menu = new Menu();
+            //Console.WriteLine("Entrar na solução.");
+            //menu.MainMenu();
+            // testar cenas
+            SceneRender sceneRender = new SceneRender();
+            sceneRender.RenderScene();
         }
     }
 }
--- Projeto2LP2/Render/SceneRender.cs
+++ Projeto2LP2/Render/SceneRender.cs
@@ -5,7 +5,36 @@ namespace Projeto2LP2
     class SceneRender
     {
         public void RenderScene() {
+            RenderMainScene();
+        }
 
+        private void RenderMainScene() {
+            //linha de cima
+            for (int i = 0; i < 35; i++) {
+                Console.SetCursorPosition(i, 0);
+                Console.WriteLine("-");
+            }
+
+            //linha de baixo
+            for (int i = 0; i < 35; i++) {
+                Console.SetCursorPosition(i, 15);
+                Console.WriteLine("-");
+            }
+
+            //linha da esquerda
+            for (int i = 0; i < 16; i++) {
+                Console.SetCursorPosition(0, i);
+                Console.WriteLine("|");
+            }
+
+            //linha da direita
+            for (int i = 0; i < 16; i++) {
+                Console.SetCursorPosition(36, i);
+                Console.WriteLine("|");
+            }
+
+            Console.SetCursorPosition(0, 17);
+            Console.WriteLine("Score: ");
         }
     }
 }
