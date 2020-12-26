0c565a9fce808193196b59e9a2b2708831096ce7
FranciscoPires <frelvaspires@gmail.com>
2020-12-26 17:16:18 +0000

    O FoodObject já usa Facade.

--- Projeto2LP2/Objects/FoodObject.cs
+++ Projeto2LP2/Objects/FoodObject.cs
@@ -12,20 +12,20 @@ namespace Projeto2LP2
 
         public override void Start() {
             // Definir a posição da comida inicial.
-            FoodX = (SceneObject.SceneX / 2) - 5;
-            FoodY = (SceneObject.SceneY / 2) - 5;
+            FoodX = (Facade.GetSceneX / 2) - 5;
+            FoodY = (Facade.GetSceneY / 2) - 5;
         }
 
         public override void Update() {
             random = new Random();
 
-            GameState.GetCordFoodX = random.Next(2, GameState.GetSceneX - 2);
-            GameState.GetCordFoodY = random.Next(2, GameState.GetSceneY - 2);
+            Facade.GetCordFoodX = random.Next(2, Facade.GetSceneX - 2);
+            Facade.GetCordFoodY = random.Next(2, Facade.GetSceneY - 2);
         }
 
         public override void Render() {
-            Console.SetCursorPosition(RenderEngine.GetFoodX, RenderEngine.GetFoodY);
-            Food food = new Food(RenderEngine.GetFoodX, RenderEngine.GetFoodY, '$');
+            Console.SetCursorPosition(Facade.GetCordFoodX, Facade.GetCordFoodY);
+            Food food = new Food(Facade.GetCordFoodX, Facade.GetCordFoodY, '$');
             Console.Write(food.FoodChar);
         }
     }
