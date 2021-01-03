72c2079150a23ef31cb2121e3bb2802ada0943dd
Francisco Pires <frelvaspires@gmail.com>
2021-01-03 17:49:32 +0000

    Erros corrigidos

--- Projeto2LP2/Program.cs
+++ Projeto2LP2/Program.cs
@@ -5,12 +5,12 @@ namespace Projeto2LP2
     /// <summary>
     /// Classe onde inicia o jogo.
     /// </summary>
-    sealed class Program
+    public static class Program
     {
         /// <summary>
         /// Começar o jogo.
         /// </summary>
-        static void Main(/*string[] args*/) {
+        private static void Main(/*string[] args*/) {
             Menu menu = new Menu();
             menu.MainMenu(true, true);
         }
